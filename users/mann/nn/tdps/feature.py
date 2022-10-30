152e0bb490515f95e130f8f90fcb2b2729eedc0f
Daniel Mann <mann@i6.informatik.rwth-aachen.de>
2022-10-30 13:52:25 +0100

    update feature tdp architectures

--- users/mann/nn/tdps/feature.py
+++ users/mann/nn/tdps/feature.py
@@ -27,44 +27,46 @@ BLSTM_CONFIG = {
     "L2": 0.0001
 }
 
-TDP_BLSTM_NO_LABEL_LAYER = lambda num_classes: {
-    "class": "subnetwork",
-    "from": ["fwd_6", "bwd_6"],
-    "subnetwork": {
-        "lstm_fwd": {
-            'class'     : 'rec',
-            'unit'      : 'lstmp',
-            'direction' : 1,
-            'n_out'     : 1,
-            **BLSTM_CONFIG,
-        },
-        "lstm_bwd": {
-            'class'     : 'rec',
-            'unit'      : 'lstmp',
-            'direction' : -1,
-            'n_out'     : 1,
-            **BLSTM_CONFIG,
-        },
-        "fwd_prob": {
-            "activation": "log_sigmoid",
-            "class": "linear",
-            "from": ["lstm_fwd", "lstm_bwd"],
-            "n_out": 1,
-        },
-        "loop_prob": {
-            "class": "eval",
-            "eval": "safe_log(1 - tf.exp(source(0)))",
-            "from": ["fwd_prob"],
-        },
-        "output": {
-            "class": "expand_dims",
-            "from": ["fwd_prob", "loop_prob"],
-            "axis": "spatial",
-            "dim": num_classes,
-        },
+def TDP_BLSTM_NO_LABEL_LAYER(num_classes, n_out=512):
+    return {
+        "class": "subnetwork",
+        "from": ["fwd_6", "bwd_6"],
+        "subnetwork": {
+            "lstm_fwd": {
+                'class'     : 'rec',
+                'unit'      : 'lstmp',
+                'direction' : 1,
+                'n_out'     : n_out,
+                **BLSTM_CONFIG,
+            },
+            "lstm_bwd": {
+                'class'     : 'rec',
+                'unit'      : 'lstmp',
+                'direction' : -1,
+                'n_out'     : n_out,
+                **BLSTM_CONFIG,
+            },
+            "fwd_prob": {
+                "activation": "log_sigmoid",
+                "class": "linear",
+                "from": ["lstm_fwd", "lstm_bwd"],
+                "n_out": 1,
+            },
+            "loop_prob": {
+                "class": "eval",
+                "eval": "safe_log(1 - tf.exp(source(0)))",
+                "from": ["fwd_prob"],
+            },
+            "output": {
+                "class": "expand_dims",
+                "from": ["fwd_prob", "loop_prob"],
+                "axis": "spatial",
+                "dim": num_classes,
+            },
+        }
     }
-}
 
+TDP_BLSTM_NO_LABEL_LAYER_SMALL = lambda num_classes: TDP_BLSTM_NO_LABEL_LAYER(num_classes, 1)
 
 TDP_SIGMOID_NO_LABEL = lambda num_classes: {
     "for_all_labels": {
@@ -125,39 +127,43 @@ TPD_BLSTM_NO_LABEL_SIGMOID_LAYER = lambda num_classes: {
     }
 }
 
-TPD_BLSTM_LAYER_SIGMOID = lambda num_classes: {
-    "class": "subnetwork",
-    "from": ["fwd_6", "bwd_6"],
-    "subnetwork": {
-        "lstm_fwd": {
-            'class'     : 'rec',
-            'unit'      : 'lstmp',
-            'direction' : 1,
-            'n_out'     : 1,
-            **BLSTM_CONFIG,
-        },
-        "lstm_bwd": {
-            'class'     : 'rec',
-            'unit'      : 'lstmp',
-            'direction' : -1,
-            'n_out'     : 1,
-            **BLSTM_CONFIG,
-        },
-        "fwd_prob": {
-            "class": "linear",
-            "activation": "log_sigmoid",
-            "from": ["lstm_fwd", "lstm_bwd"],
-            "n_out": num_classes,
-        },
-        **TDP_SIGMOID.copy(),
+def TDP_BLSTM_LAYER_SIGMOID(num_classes, n_out=512):
+    return {
+        "class": "subnetwork",
+        "from": ["fwd_6", "bwd_6"],
+        "subnetwork": {
+            "lstm_fwd": {
+                'class'     : 'rec',
+                'unit'      : 'lstmp',
+                'direction' : 1,
+                'n_out'     : n_out,
+                **BLSTM_CONFIG,
+            },
+            "lstm_bwd": {
+                'class'     : 'rec',
+                'unit'      : 'lstmp',
+                'direction' : -1,
+                'n_out'     : n_out,
+                **BLSTM_CONFIG,
+            },
+            "fwd_prob": {
+                "class": "linear",
+                "activation": "log_sigmoid",
+                "from": ["lstm_fwd", "lstm_bwd"],
+                "n_out": num_classes,
+            },
+            **TDP_SIGMOID.copy(),
+        }
     }
-}
+
+TDP_BLSTM_LAYER_SIGMOID_SMALL = lambda num_classes: TDP_BLSTM_LAYER_SIGMOID(num_classes, n_out=1)
 
 FEATURE_ARCHS = {
     'blstm_no_label': TDP_BLSTM_NO_LABEL_LAYER,
     "ffnn": TDP_FFNN_LAYER,
     "blstm_no_label_sigmoid": TPD_BLSTM_NO_LABEL_SIGMOID_LAYER,
-    "blstm": TPD_BLSTM_LAYER_SIGMOID,
+    "blstm": TDP_BLSTM_LAYER_SIGMOID,
+    "blstm_large": TDP_BLSTM_LAYER_SIGMOID,
 }
 
 def get_tdp_layer(num_classes, arch: Arch="ffnn"):
@@ -181,12 +187,15 @@ TDP_OUTPUT_LAYER_W_SOFTMAX = {
     "loss_opts": {'align_layer': 'fast_bw/tdps', 'loss_wrt_to_act_in': 'log_softmax'}
 }
 
+from functools import partial
+
 FEATURE_MODEL_BUILDERS = {
-    # 'blstm_no_label': TdpModelBuilder(TDP_BLSTM_NO_LABEL_LAYER, TDP_OUTPUT_LAYER_AS_FUNC),
     'ffnn': TdpModelBuilder(TDP_FFNN_LAYER, TDP_OUTPUT_LAYER_AS_FUNC),
     'blstm_no_label_sigmoid': TdpModelBuilder(TPD_BLSTM_NO_LABEL_SIGMOID_LAYER, TDP_OUTPUT_LAYER_AS_FUNC),
-    'blstm': TdpModelBuilder(TPD_BLSTM_LAYER_SIGMOID, TDP_OUTPUT_LAYER_AS_FUNC),
-    'blstm_no_label': TdpModelBuilder(TDP_BLSTM_NO_LABEL_LAYER, TDP_OUTPUT_LAYER_AS_FUNC)
+    'blstm': TdpModelBuilder(TDP_BLSTM_LAYER_SIGMOID_SMALL, TDP_OUTPUT_LAYER_AS_FUNC),
+    'blstm_no_label': TdpModelBuilder(TDP_BLSTM_NO_LABEL_LAYER_SMALL, TDP_OUTPUT_LAYER_AS_FUNC),
+    'blstm_large': TdpModelBuilder(TDP_BLSTM_LAYER_SIGMOID, TDP_OUTPUT_LAYER_AS_FUNC),
+    'blstm_no_label_large': TdpModelBuilder(TDP_BLSTM_NO_LABEL_LAYER, TDP_OUTPUT_LAYER_AS_FUNC),
 }
 
 SOFTMAX_ARCHS = [
